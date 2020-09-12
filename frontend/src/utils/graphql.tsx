79d76aef4c41e9bfa90f55b5a848f01ae6bfd244
kannika klankhiew <poppyhtd44@gmail.com>
2020-09-12 14:14:55 +0700

    no message

--- frontend/src/utils/graphql.tsx
+++ frontend/src/utils/graphql.tsx
@@ -1,9 +1,59 @@
 import { gql } from 'apollo-boost'
 // const { makeExecutableSchema, gql } = require('apollo-server');
-export const GET_USERS = gql`
+// export const GET_USERS = gql`
+//   query {
+//     users{
+//         id
+//     }
+//   }
+// `
+export const FIND_JOB_TYPES = gql`
   query {
-    users{
-        id
+    jobTypes{
+      id
+      createdAt
+      updatedAt
+      jobTypeName
+    }
+  }
+`
+
+export const CREATE_JOB = gql`
+  mutation createJob(
+    $userId: String!, 
+    $jobName: String!, 
+    $jobTypeId: String!, 
+    $startJob: DateTime,
+    $endJob: DateTime,
+    $location: String!,
+    $guest: Int,
+    $detail: String,
+    $startBudget: Float!,
+    $endBudget: Float!,
+    $tel: String!,
+    $email: String!,
+    $limit: DateTime,
+    $displayName: String!,
+    $lineEmail: String!
+  ){
+    createJob(
+      userId: $userId,
+      jobName: $jobName,
+      jobTypeId: $jobTypeId,
+      startJob: $startJob,
+      endJob: $endJob,
+      location: $location,
+      guest: $guest,
+      detail: $detail,
+      startBudget: $startBudget,
+      endBudget: $endBudget,
+      tel: $tel,
+      email: $email,
+      limit: $limit,
+      displayName: $displayName,
+      lineEmail: $lineEmail,
+    ){
+      id
     }
   }
 `
