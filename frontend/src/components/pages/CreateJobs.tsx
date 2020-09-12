4330c3e7fb11e7d7da0dcbc0d188a1784593ca1f
kannika klankhiew <poppyhtd44@gmail.com>
2020-09-12 16:15:48 +0700

    update CreateJobs

--- frontend/src/components/pages/CreateJobs.tsx
+++ frontend/src/components/pages/CreateJobs.tsx
@@ -21,20 +21,35 @@ import liffHelper from '../../utils/liffHelper';
 import { FIND_JOB_TYPES, CREATE_JOB } from '../../utils/graphql';
 
 const timeFormat = 'HH:mm';
+const dateFormat = 'YYYY-MM-DD';
+const dateTimeFormat = 'YYYY-MM-DD HH:mm';
 const { TextArea } = Input;
 
 const CreateJobs = () => {
   // line user id poppy: U1a85d09a5b0f102277500c1f1b2026a8
   const [userId, setUserId] = useState('')
+  const [userDisplayName, setUserDisplayName] = useState('')
   liffHelper.getProfile()
     .then(profile => {
       console.log('profile', profile)
-      window.alert(JSON.stringify(profile))
+      // window.alert(JSON.stringify(profile))
       setUserId(profile.userId)
+      setUserDisplayName(profile.displayName)
     });
   
-  const { loading: jobloading, error, data: jobtype } = useQuery(FIND_JOB_TYPES)
-  console.log(jobtype);
+  // const { loading: jobloading, error, data: jobtype } = useQuery(FIND_JOB_TYPES,{fetchPolicy: 'network-only'})
+  // console.log(jobtype);
+  const jobType = [{id: '1', jobTypeName: 'แต่งงาน'},{id: '2', jobTypeName:'pre wedding'}]
+
+  const [CREATE_JOB_QUERY, { loading, data}] = useMutation(CREATE_JOB, {
+    // fetchPolicy: 'network-only',
+    onCompleted:(sre)=>{
+      window.alert('success')
+    },
+    onError: (err) => {
+      window.alert(err)
+    }
+  });
   // const [sendMessage, { called, loading, data }] = useLazyQuery(
   //   SEND_MESSAGE
   // );
@@ -49,38 +64,30 @@ const CreateJobs = () => {
   // });
 
   const onFinish = (values:any) => {
+    // ***** ยังไม่ได้ validate form
     console.log(values);
+    var sJob = moment(values.startJob.date)
+    var eJob = moment(values.endJob.date)
+    var jobName = `งาน${jobType.filter(v => v.id == values.jobType)[0].jobTypeName} ${eJob.diff(sJob, 'days')} วัน`
+
     CREATE_JOB_QUERY({variables:{
       userId: userId, 
-      // $jobName: String!, 
-      // $jobTypeId: String!, 
-      // $startJob: DateTime,
-      // $endJob: DateTime,
-      // $location: String!,
-      // $guest: Int,
-      // $detail: String,
-      // $startBudget: Float!,
-      // $endBudget: Float!,
-      // $tel: String!,
-      // $email: String!,
-      // $limit: DateTime,
-      // $displayName: String!,
-      // $lineEmail: String!
+      jobName: jobName, 
+      jobTypeId: values.jobType, 
+      startJob: `${moment(values.startJob.date).format(dateFormat)} ${moment(values.startJob.time || '00:00', timeFormat).format(timeFormat)}`,
+      endJob: `${moment(values.endJob.date).format(dateFormat)} ${moment(values.endJob.time || '00:00', timeFormat).format(timeFormat)}`,
+      location: values.location || `{lat: 0,log: 0}`,
+      guest: values.guest || 0,
+      detail: values.detail || '',
+      startBudget: values.startBudget || 0,
+      endBudget: values.endBudget || 0,
+      tel: values.tel || '',
+      email: values.email || '',
+      limit: moment().add(values.limit || 1, 'days').format(dateTimeFormat),
+      displayName: userDisplayName,
+      lineEmail: ''
     }})
-    // ***** ยังไม่ได้ validate form
   };
-
-  const [CREATE_JOB_QUERY, { loading, data}] = useMutation(CREATE_JOB, {
-    // fetchPolicy: 'network-only',
-    onCompleted:(sre)=>{
-      window.alert('success')
-    },
-    onError: (err) => {
-      window.alert(err)
-    }
-  });
-
-  // const jobType = ['wedding','pre wedding']
 
   return (
     <body>
@@ -88,20 +95,21 @@ const CreateJobs = () => {
       <div style={{ margin: '1.5rem' }}>
         <p style={{ fontSize: '1.5em', fontWeight: 'bold', marginTop: '1rem', marginBottom: '0' }}>กรุณาระบุรายละเอียดงานของคุณ</p>
         <Form onFinish={onFinish}>
-          {JSON.stringify(jobtype)}
             <Form.Item label="ประเภทงาน"
             name="jobType"
-            rules={[{ required: true, message: 'กรุณาเลือกประเภทงาน' }]}>
+            rules={[{ required: true, message: 'กรุณาเลือกประเภทงาน' }]}
+            >
             <Select placeholder="กรุณาเลือกประเภทของงาน">
-              {/* {jobtype.map((option) => (
+              {jobType.map((option) => (
                 <Select.Option key={option.id} value={option.id}>{option.jobTypeName}</Select.Option>
-              ))} */}
+              ))}
             </Select>
             </Form.Item>
             <Form.Item label="วันเริ่มงาน">
             <Input.Group compact>
               <Form.Item name={['startJob', 'date']}
-              rules={[{ required: true, message: 'กรุณาระบุวันเริ่มงาน' }]}>
+              rules={[{ required: true, message: 'กรุณาระบุวันเริ่มงาน' }]}
+              >
                 <DatePicker onChange={()=>{}} disabledDate={d => !d || d.isBefore(Date.now())} />
               </Form.Item>
               <Form.Item name={['startJob', 'time']}>
@@ -112,7 +120,8 @@ const CreateJobs = () => {
             <Form.Item label="วันสิ้นสุดงาน">
               <Input.Group compact>
                 <Form.Item name={['endJob', 'date']}
-                  rules={[{ required: true, message: 'กรุณาระบุวันสิ้นสุดงาน' }]}>
+                  rules={[{ required: true, message: 'กรุณาระบุวันสิ้นสุดงาน' }]}
+                  >
                   <DatePicker onChange={()=>{}} />
                 </Form.Item>
                 <Form.Item name={['endJob', 'time']}>
@@ -122,7 +131,8 @@ const CreateJobs = () => {
             </Form.Item>
             <Form.Item label="สถานที่จัดงาน / ห้อง"
             name="location"
-            rules={[{ required: true, message: 'กรุณาระบุข้อมูลสถานที่ และห้องจัดงาน' }]}>
+            rules={[{ required: true, message: 'กรุณาระบุข้อมูลสถานที่ และห้องจัดงาน' }]}
+            >
               <Input placeholder="กรุณาระบุข้อมูลสถานที่ และห้องจัดงาน" />
             </Form.Item>
             <Form.Item label="จำนวนแขก"
@@ -136,7 +146,8 @@ const CreateJobs = () => {
             <Form.Item label="งบประมาณ" >
               <Input.Group compact>
                 <Form.Item name="startBudget"
-                rules={[{ required: true, message: 'กรุณาระบุราคาต่ำสุด' }]}>
+                // rules={[{ required: true, message: 'กรุณาระบุราคาต่ำสุด' }]}
+                >
                   <Input 
                     style={{ width: 140, textAlign: 'center' }} 
                     suffix="฿" 
@@ -145,7 +156,8 @@ const CreateJobs = () => {
                 </Form.Item>
                 <Input style={{ width: 30, borderLeft: 0, borderRight: 0, pointerEvents: 'none', }} placeholder="-" disabled />
                 <Form.Item name="endBudget"
-                rules={[{ required: true, message: 'กรุณาระบุงบราคาสูงสุด' }]}>
+                // rules={[{ required: true, message: 'กรุณาระบุงบราคาสูงสุด' }]}
+                >
                   <Input
                     style={{
                       width: 140,
@@ -162,7 +174,8 @@ const CreateJobs = () => {
             <p style={{ fontSize: '1.5em', fontWeight: 'bold', marginTop: '1rem', marginBottom: '0' }}>ข้อมูลการติดต่อ</p>
             <Form.Item label="เบอร์โทร"
             name="tel"
-            rules={[{ required: true, message: 'กรุณาระบุเบอร์โทร' }]}>
+            rules={[{ required: true, message: 'กรุณาระบุเบอร์โทร' }]}
+            >
               <Input type="number" placeholder="กรุณาระบุเบอร์โทร" />
             </Form.Item>
             <Form.Item label="อีเมล"
@@ -173,7 +186,8 @@ const CreateJobs = () => {
             <p style={{ fontSize: '1.5em', fontWeight: 'bold', marginTop: '1rem', marginBottom: '0' }}>ระยะเวลาในการหาช่างภาพ</p>
             <Form.Item label="ต้องการหาช่างภาพให้ได้ (วัน)"
             name="limit"
-            rules={[{ required: true, message: 'กรุณาระบุจำนวนวัน' }]}>
+            rules={[{ required: true, message: 'กรุณาระบุจำนวนวัน' }]}
+            >
               <Input type="number" style={{textAlign: 'center'}} defaultValue="1" prefix="ภายใน" suffix="วัน"  />
             </Form.Item>
             <p style={{ fontSize: '0.8em', textAlign: 'center'}}> ข้อมูลสำคัญ กรุณากรอกให้ครบถ้วน</p>
